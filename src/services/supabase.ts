724ec564c05fc6484926b1c364fb7ce9342770b6
Rachelle Zhu <darkd7131@gmail.com>
2024-12-10 21:14:11 +0700

    implement dot env

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/supabase-type";
 
-export const supabaseUrl: string = "https://kzhoholumyvgzkmyrfzr.supabase.co";
-const supabaseKey: string =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6aG9ob2x1bXl2Z3prbXlyZnpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc1MzQ5NjUsImV4cCI6MjAzMzExMDk2NX0.WqucySvK3pfnFMBlUwtMskFkRjDj7MX8oIiSYWEWnCQ";
+export const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_PROJECT_KEY;
+
 // Create a single supabase client for interacting with your database
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_PROJECT_KEY: string;
+}
+
+interface ImpotMeta {
+  readonly env: ImportMetaEnv;
+}
