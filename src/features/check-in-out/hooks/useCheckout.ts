c42e0f30fc8fa666bf28c5385f456c0ac9734969
Rachalle Zhu <darkd7131@gmail.com>
2024-12-11 05:45:35 +0700

    add checkout feature

---
+++ src/features/check-in-out/hooks/useCheckout.ts
@@ -0,0 +1,34 @@
+import { UseMutateFunction, useMutation, useQueryClient } from "react-query";
+import { updateBooking } from "../../../services/apiBookings";
+import toast from "react-hot-toast";
+import { Tables } from "../../../types/supabase-type";
+
+type UseCheckoutType = {
+  isCheckingOut: boolean;
+  checkout: UseMutateFunction<
+    Tables<"bookings">,
+    unknown,
+    Tables<"bookings">["id"],
+    unknown
+  >;
+};
+
+export function useCheckout(): UseCheckoutType {
+  const queryClient = useQueryClient();
+
+  const { mutate: checkout, isLoading: isCheckingOut } = useMutation({
+    mutationFn: (bookingId: number) =>
+      updateBooking(bookingId, {
+        status: "checked-out",
+      }),
+    onSuccess: (data: Tables<"bookings">) => {
+      toast.success(`Booking #${data.id} successfully checked out`);
+      queryClient.invalidateQueries();
+    },
+    onError: () => {
+      toast.error("There was an error while checking out");
+    },
+  });
+
+  return { checkout, isCheckingOut };
+}
