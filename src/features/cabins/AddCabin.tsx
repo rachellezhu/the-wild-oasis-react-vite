3f30c6ea106eac7fbb6733672933e708b04194e3
Rachelle Zhu <darkd7131@gmail.com>
2024-12-31 17:25:28 +0700

    remove unused files

--- src/features/cabins/AddCabin.tsx
+++ src/features/cabins/AddCabin.tsx
@@ -3,23 +3,6 @@ import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
-// export default function AddCabin(): React.ReactElement {
-//   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
-
-//   return (
-//     <div>
-//       <Button onClick={() => setIsOpenModal((show) => !show)}>
-//         Add new cabin
-//       </Button>
-//       {isOpenModal && (
-//         <Modal onClose={() => setIsOpenModal(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-//         </Modal>
-//       )}
-//     </div>
-//   );
-// }
-
 export default function AddCabin(): React.ReactElement {
   return (
     <div>
@@ -30,13 +13,6 @@ export default function AddCabin(): React.ReactElement {
         <Modal.Window name="cabin-form">
           <CreateCabinForm />
         </Modal.Window>
-
-        {/* <Modal.Open opens="table">
-        <Button>Open table</Button>
-        </Modal.Open>
-        <Modal.Window name="table">
-        <CabinTable />
-        </Modal.Window> */}
       </Modal>
     </div>
   );
